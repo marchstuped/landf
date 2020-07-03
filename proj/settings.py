dfe38869b9ac39b7980a992975d910c992049b66
Nattanich <nattanich.su@ku.th>
2020-07-03 13:37:05 +0700

    deploy1

--- proj/settings.py
+++ proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -157,3 +158,6 @@ PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'en-US'
 
 PWA_APP_DEBUG_MODE = False
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
